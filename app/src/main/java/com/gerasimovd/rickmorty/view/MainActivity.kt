ab7ab926f9d1116d77a34151a9a4f0e56736044a
Daniil Gerasimov <Dan>
2021-10-05 06:08:01 +0400

    - Added splash screen and icon

--- app/src/main/java/com/gerasimovd/rickmorty/view/MainActivity.kt
+++ app/src/main/java/com/gerasimovd/rickmorty/view/MainActivity.kt
@@ -41,6 +41,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_RickMorty)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setBottomNavListener()
